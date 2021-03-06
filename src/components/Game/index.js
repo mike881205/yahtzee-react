f79866c58e782f61bd77bc005d56dbda31379672
mike881205 <mike881205@gmail.com>
2021-03-06 14:17:54 -0500

    updated setState functions to return point values for hands

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -31,12 +31,12 @@ class Game extends Component {
         diceSlotChildren: [],
         scoreBoard: [
             [
-                { handName: "Ones", available: true, validHand: false, value: 1, points: 0 },
-                { handName: "Twos", available: true, validHand: false, value: 2, points: 0 },
-                { handName: "Threes", available: true, validHand: false, value: 3, points: 0 },
-                { handName: "Fours", available: true, validHand: false, value: 4, points: 0 },
-                { handName: "Fives", available: true, validHand: false, value: 5, points: 0 },
-                { handName: "Sixes", available: true, validHand: false, value: 6, points: 0 }
+                { handName: "Ones", available: true, qty: 0, value: 1, points: 0 },
+                { handName: "Twos", available: true, qty: 0, value: 2, points: 0 },
+                { handName: "Threes", available: true, qty: 0, value: 3, points: 0 },
+                { handName: "Fours", available: true, qty: 0, value: 4, points: 0 },
+                { handName: "Fives", available: true, qty: 0, value: 5, points: 0 },
+                { handName: "Sixes", available: true, qty: 0, value: 6, points: 0 }
             ],
             [
                 { handName: "Three-of-a-Kind", available: true, validHand: false, points: 0 },
@@ -146,6 +146,8 @@ class Game extends Component {
         let roll = this.state.roll
         let gameOver = this.state.gameOver
         let roundOver = this.state.roundOver
+        let scoreBoard = this.state.scoreBoard
+        let handChildrenLeft = []
 
         if (gameOver) {
             gameOver = false
@@ -176,10 +178,24 @@ class Game extends Component {
                     />
                 )
             }
+            for (let j = 0; j < scoreBoard[0].length; j++) {
+                scoreBoard[0][j].qty = 0
+                handChildrenLeft.push(
+                    <ScoreBoardRow
+                        key={j+"Left"}
+                        id={j + "Left"}
+                        hand={this.state.scoreBoard[0][j].handName}
+                        points={0}
+                    />
+                )
+            }
+
             this.setState({
                 roll: roll,
                 roundOver: roundOver,
-                diceSlotChildren: diceSlotChildren
+                diceSlotChildren: diceSlotChildren,
+                scoreBoard: scoreBoard,
+                handChildrenLeft: handChildrenLeft
             })
         }
         else {
@@ -238,15 +254,17 @@ class Game extends Component {
 
                     finalValues.push(diceSlots[i].value)
                 }
+
                 this.calcHand(finalValues)
+
+                this.setState({
+                    diceSlots: diceSlots,
+                    diceSlotChildren: diceSlotChildren,
+                    roll: roll,
+                    gameOver: gameOver,
+                    roundOver: roundOver
+                })
             }
-            this.setState({
-                diceSlots: diceSlots,
-                diceSlotChildren: diceSlotChildren,
-                roll: roll,
-                gameOver: gameOver,
-                roundOver: roundOver
-            })
         }
     }
 
@@ -318,92 +336,105 @@ class Game extends Component {
 
         let straights = []
         let scoreBoard = this.state.scoreBoard
+        let handChildrenLeft = []
 
         values.sort(function (a, b) {
             return a - b;
         });
 
-        // values = [2, 3, 4, 5, 6]
-
         console.log(values)
-
-        // Find Duplicates
-
-        let duplicates = values.filter((item, index) => values.indexOf(item) !== index)
-        let duplicates2 = duplicates.filter((item, index) => duplicates.indexOf(item) !== index)
-
-        switch (duplicates.length) {
-            case 2:
-                if (duplicates2.length === 1) {
-                    scoreBoard[1][0].validHand = true
-                }
-                break;
-            case 3:
-                if (duplicates2.length === 1) {
-                    scoreBoard[1][0].validHand = true
-                    scoreBoard[2][0].validHand = true
-                }
-                else {
-                    scoreBoard[1][0].validHand = true
-                    scoreBoard[1][1].validHand = true
-                }
-                break;
-            case 4:
-                scoreBoard[1][0].validHand = true
-                scoreBoard[1][1].validHand = true
-                scoreBoard[3][1].validHand = true
-                break;
-        }
-
-        // Find Straights
-
-        if (!scoreBoard[1][0].validHand) {
-
-            for (let i = 0; i < values.length - 1; i++) {
-                if (values[i + 1] - values[i] === 1) {
-                    straights.push(values[i], values[i + 1])
-                }
-            }
-
-            let strtBeforeSplice = straights
-            console.log(strtBeforeSplice)
-
-            for (let i = 0; i < straights.length; i++) {
-                if (straights[i] === straights[i+1]) {
-                    straights.splice(i, 1)
-                }
-            }
-
-            let strtAfterSplice = straights
-            console.log(strtAfterSplice)
-
-            if (strtAfterSplice.length === 4 && strtAfterSplice !== strtBeforeSplice) {
-                scoreBoard[2][1].validHand = true
-            }
-            else if (strtAfterSplice.length === 5 && strtAfterSplice == strtBeforeSplice) {
-                scoreBoard[2][1].validHand = true
-                scoreBoard[2][2].validHand = true
-            }
-        }
 
         // Find side values
 
         for (let i = 0; i < values.length; i++) {
             for (let j = 0; j < scoreBoard[0].length; j++) {
                 if (values[i] === scoreBoard[0][j].value) {
-                    scoreBoard[0][j].validHand = true
+                    scoreBoard[0][j].qty++
                 }
             }
         }
 
-        console.log("Valid Hands")
-        for (let i = 0; i < scoreBoard.length; i++) {
-            for (let j = 0; j < scoreBoard[i].length; j++) {
-                if (scoreBoard[i][j].validHand) {
-                    console.log(scoreBoard[i][j].handName)
-                }
-            }
+        for (let i = 0; i < scoreBoard[0].length; i++) {
+            scoreBoard[0][i].points = scoreBoard[0][i].value * scoreBoard[0][i].qty
+            handChildrenLeft.push(
+                <ScoreBoardRow
+                    key={i+"Left"}
+                    id={i + "Left"}
+                    hand={this.state.scoreBoard[0][i].handName}
+                    points={scoreBoard[0][i].points}
+                />
+            )
         }
+
+        this.setState({scoreBoard: scoreBoard, handChildrenLeft: handChildrenLeft})
+
+        // // Find Duplicates
+
+        // let duplicates = values.filter((item, index) => values.indexOf(item) !== index)
+        // let duplicates2 = duplicates.filter((item, index) => duplicates.indexOf(item) !== index)
+
+        // switch (duplicates.length) {
+        //     case 2:
+        //         if (duplicates2.length === 1) {
+        //             scoreBoard[1][0].validHand = true
+        //         }
+        //         break;
+        //     case 3:
+        //         if (duplicates2.length === 1) {
+        //             scoreBoard[1][0].validHand = true
+        //             scoreBoard[2][0].validHand = true
+        //         }
+        //         else {
+        //             scoreBoard[1][0].validHand = true
+        //             scoreBoard[1][1].validHand = true
+        //         }
+        //         break;
+        //     case 4:
+        //         scoreBoard[1][0].validHand = true
+        //         scoreBoard[1][1].validHand = true
+        //         scoreBoard[3][1].validHand = true
+        //         break;
+        // }
+
+        // // Find Straights
+
+        // if (!scoreBoard[1][0].validHand) {
+
+        //     for (let i = 0; i < values.length - 1; i++) {
+        //         if (values[i + 1] - values[i] === 1) {
+        //             straights.push(values[i], values[i + 1])
+        //         }
+        //     }
+
+        //     let strtBeforeSplice = straights
+        //     console.log(strtBeforeSplice)
+
+        //     for (let i = 0; i < straights.length; i++) {
+        //         if (straights[i] === straights[i + 1]) {
+        //             straights.splice(i, 1)
+        //         }
+        //     }
+
+        //     let strtAfterSplice = straights
+        //     console.log(strtAfterSplice)
+
+        //     if (strtAfterSplice.length === 4 && strtAfterSplice !== strtBeforeSplice) {
+        //         scoreBoard[2][1].validHand = true
+        //     }
+        //     else if (strtAfterSplice.length === 5 && strtAfterSplice == strtBeforeSplice) {
+        //         scoreBoard[2][1].validHand = true
+        //         scoreBoard[2][2].validHand = true
+        //     }
+        // }
+
+        // console.log("Valid Hands")
+        // for (let i = 0; i < scoreBoard.length; i++) {
+        //     for (let j = 0; j < scoreBoard[i].length; j++) {
+        //         if (scoreBoard[i][j].validHand) {
+        //             console.log(scoreBoard[i][j].handName)
+        //         }
+        //     }
+        // }
     }
 
     render() {
